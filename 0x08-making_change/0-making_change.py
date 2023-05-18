1b0c6738289d27c617fd2847b3f0435941cadef1
Ernyson <ernestaiji1@gmail.com>
2023-05-18 16:18:00 +0000

    making change interview quest siolved' '

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""
-Given a pile of coins of different values,
+"""Given a pile of coins of different values,
 determine the fewest number of coins needed
 to meet a given amount total
 """
 
 
 def makeChange(coins, total):
+    """make change
     """
-    Now determines the fewest number of coins needed to meet a given
-    amount total
-    """
-    if (total <= 0):
+    if total <= 0:
         return 0
-
-    f_coins = [None] * (total + 1)
-    f_coins[0] = 0
-    for i in range(1, total + 1):
-        f_coins[i] = total + 1
-    
-    for coin in coins:
-        for i in range(coin, total + 1):
-            if f_coins[i - coin] + 1 < f_coins[i]:
-                f_coins[i] = f_coins[i - coin] + 1
-
-    if f_coins[total] > total:
-        return -1
-    else:
-        return f_coins[total]
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
